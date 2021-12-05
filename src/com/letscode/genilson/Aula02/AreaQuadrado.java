eb92a828a70ff2fd07212269f353f6ca562bfe82
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-12-05 17:01:23 -0300

    Aula 02 - Let's Code

---
+++ src/com/letscode/genilson/Aula02/AreaQuadrado.java
@@ -0,0 +1,8 @@
+package com.letscode.genilson.Aula02;
+
+public class AreaQuadrado {
+    int lado1 = 2;
+    int lado2 = 4;
+    int areaDoQuadrado = lado1 * lado2;
+
+}
---
+++ src/com/letscode/genilson/Aula02/DaysOfWeek.java
@@ -0,0 +1,8 @@
+package com.letscode.genilson.Aula02;
+
+public enum DaysOfWeek {
+
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
+    //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado;
+
+}
