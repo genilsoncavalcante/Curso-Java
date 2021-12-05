78645db32af718c83ce1eba3cc5db2979ceae057
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2021-12-05 17:18:21 -0300

    Delete src/com/letscode/genilson directory

--- src/com/letscode/genilson/Aula02/AreaQuadrado.java
+++
@@ -1,8 +0,0 @@
-package com.letscode.genilson.Aula02;
-
-public class AreaQuadrado {
-    int lado1 = 2;
-    int lado2 = 4;
-    int areaDoQuadrado = lado1 * lado2;
-
-}
--- src/com/letscode/genilson/Aula02/DaysOfWeek.java
+++
@@ -1,8 +0,0 @@
-package com.letscode.genilson.Aula02;
-
-public enum DaysOfWeek {
-
-    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
-    //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado;
-
-}
